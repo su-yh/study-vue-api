b5ff8db597ebdb15c388deeffab5f0ef3b4b33bf
suyh <suyunhong@ater-info.com>
2024-12-31 14:30:35 +0800

    不用MQ

--- src/main/java/com/eb/mq/consumer/dto/MqConsumerBaseDto.java
+++
@@ -1,8 +0,0 @@
-package com.eb.mq.consumer.dto;
-
-/**
- * @author suyh
- * @since 2024-09-10
- */
-public interface MqConsumerBaseDto {
-}
--- src/main/java/com/eb/mq/product/dto/MqProductBaseDto.java
+++
@@ -1,8 +0,0 @@
-package com.eb.mq.product.dto;
-
-/**
- * @author suyh
- * @since 2024-09-07
- */
-public interface MqProductBaseDto {
-}
